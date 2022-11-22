b6c36455ce135a7ffb03b5bdf4f00fb9eeb349e1
ale-d <ale-danilyan@yandex.ru>
2022-11-22 18:01:07 +0300

    .

--- test_single_file.py
+++ test_single_file.py
@@ -5,10 +5,13 @@ import librosa
 audio_model = utils.PredictGenderNoise('gender_noise_model.h5')
 
 # устанавливаем путь к файлу
-file_path = 'test_audio/noise_2.wav'
+file_path = 'test_audio/female_1.wav'
 
 # получаем сигнал
 file, _ = librosa.load(file_path, sr=8000)
+print('data after librosa', file[:10])
+print(type(file))
+print(type(file[0]))
 
 # подаем сигнал в метод analyze нашего экземпляра класса
 predict = audio_model.analyze(file)
